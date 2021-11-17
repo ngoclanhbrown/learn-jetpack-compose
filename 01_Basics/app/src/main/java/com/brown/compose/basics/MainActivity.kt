0847cdc7898fbe7470a29dde5bc7a2bb8fc4eaa0
ngoclanhbrown <tnlanh99@gmail.com>
2021-11-17 20:13:25 +0700

    Add MyApp, Greeting composable

--- 01_Basics/app/src/main/java/com/brown/compose/basics/MainActivity.kt
+++ 01_Basics/app/src/main/java/com/brown/compose/basics/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.brown.compose.basics
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.brown.compose.basics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,10 +23,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
@@ -28,14 +32,50 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Surface(color = MaterialTheme.colors.background) {
+        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+            names.forEach { name ->
+                Greeting(name = name)
+            }
+        }
+    }
+}
+
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1F)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
+        }
+    }
 }
 
 
 @Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun DefaultPreview() {
+fun PreviewMyApp() {
+    ComposeBasicsTheme {
+        MyApp()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewGreeting() {
     ComposeBasicsTheme {
         Greeting("Android")
     }
